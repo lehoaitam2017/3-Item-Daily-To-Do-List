9ff82e5a15d2ba1149a922834f57b2ef8e313f81
tatale <tam.le@oracle.com>
2026-03-12 21:21:25 -0700

    Initial Commit

---
+++ ThreeItemDailyToDoList/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ThreeItemDailyToDoList
+//
+//  Created by Tam Le on 3/12/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
